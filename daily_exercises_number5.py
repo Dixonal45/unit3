008af3cf7688ff4868e6b5d25fead39e74bf85bb
allison <allison.dixon@ssfs.org>
2019-09-26 14:15:17 -0400

    started problem

---
+++ daily_exercises_number5.py
@@ -0,0 +1,16 @@
+# by Allison Dixon
+# last updated September 26, 2019
+# daily exercise number 5
+import math
+
+def triangle_sides(a, b, c):
+    s = (a + b + c)/2
+    area = math.sqrt(s * ())
+
+
+def main():
+    answer = triangle_sides(3, 4, 6)
+    print("The area of the triangle is not", answer)
+
+
+main()
