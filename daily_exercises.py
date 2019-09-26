945255a38d73fcf702f3b87d0574b00621965172
allison <allison.dixon@ssfs.org>
2019-09-26 13:34:37 -0400

    Initial commit

---
+++ daily_exercises.py
@@ -0,0 +1,69 @@
+# by Allison Dixon
+# last updated September 25, 2019
+# daily function exercises
+
+# This program creates egg.py
+
+
+def make_top_of_hexagon():
+    print("   ________   ")
+    print("  /        \\ ")
+    print(" /          \\")
+
+
+def make_bottom_of_hexagon():
+    print(" \\          / ")
+    print("  \\________/  ")
+
+
+def make_quote_lines():
+    print(" _\"_\'_\"_\'_\"_")
+
+
+make_top_of_hexagon()
+make_bottom_of_hexagon()
+make_quote_lines()
+
+print("        ")
+make_top_of_hexagon()
+make_bottom_of_hexagon()
+make_quote_lines()
+make_bottom_of_hexagon()
+
+make_top_of_hexagon()
+make_quote_lines()
+make_bottom_of_hexagon()
+
+# This program prints happy birthday
+
+
+def happy_birthday():
+    print("Happy Birthday to you")
+
+
+def dear_name(x):
+    print("Happy Birthday dear", x)
+
+
+happy_birthday()
+happy_birthday()
+dear_name("Scoutie")
+happy_birthday()
+
+# This program adds two numbers together
+
+
+def add():
+    x = input("Give me a number")
+    y = input("Give me another number")
+    z = x + y
+    print("The numbers you chose, " + x + " and " + y + " make a sum of " + z + " !")
+
+
+
+
+
+
+
+
+
